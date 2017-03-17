f5451f5c47e584c58e8c3451f78f797bfa154891
Jorge Heleno <jorge.heleno@rnl.tecnico.ulisboa.pt>
2017-03-17 15:07:45 +0000

    Add login screen with registration

--- android-project/app/src/main/java/pt/ulisboa/tecnico/ist/cmu/locmess/MainActivity.java
+++ android-project/app/src/main/java/pt/ulisboa/tecnico/ist/cmu/locmess/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
     public void register(View v){
         Intent i = new Intent(this,RegisterActivity.class);
         startActivity(i);
+    }
+
+    public void login(View v){
+        EditText text = (EditText)findViewById(R.id.username_input);
+        System.out.println(((EditText)findViewById(R.id.username_input)).getText().toString());
+        /*This should just actually do some checks with the server in an assyncthread but for now it's k babe*/
+        if(((EditText)findViewById(R.id.username_input)).getText().toString().equals("Donald") &&
+        ((EditText)findViewById(R.id.username_input)).getText().toString().equals("America" )){
+            Intent i=new Intent(this,LocationsMenuActivity.class);
+            startActivity(i);
+        }
     }
 }
