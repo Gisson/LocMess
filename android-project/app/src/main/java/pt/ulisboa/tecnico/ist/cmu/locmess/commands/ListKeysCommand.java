9f6236aaba5393525f128fa89009013b4de2197c
Jorge Heleno <jorge.heleno@rnl.tecnico.ulisboa.pt>
2017-04-04 15:14:09 +0100

    Adds ListKeysCommand
    Refers to #31

---
+++ android-project/app/src/main/java/pt/ulisboa/tecnico/ist/cmu/locmess/commands/ListKeysCommand.java
@@ -0,0 +1,43 @@
+package pt.ulisboa.tecnico.ist.cmu.locmess.commands;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import pt.ulisboa.tecnico.ist.cmu.locmess.exception.AlreadyRequestedException;
+
+/**
+ * Created by jorge on 03/04/17.
+ */
+
+public class ListKeysCommand extends AbstractCommand {
+
+
+    private static final String _endpoint="listKeys";
+    private HashMap<String,String> _results=null;
+
+    public ListKeysCommand(String token) {
+        super(_endpoint,"token="+token);
+    }
+
+    @Override
+    public void execute() throws IOException, AlreadyRequestedException {
+        super.execute();
+    }
+
+    public Map<String,String> getResults() throws IOException, AlreadyRequestedException, JSONException {
+        if(_results==null){
+            JSONObject obj=new JSONObject(getResponse());
+            JSONArray arr=obj.getJSONArray("keys");
+            _results=new HashMap<String,String>();
+            for(int i=0; i<arr.length(); i++){
+                _results.put(arr.getJSONObject(i).getString("key"),arr.getJSONObject(i).getString("value"));
+            }
+        }
+        return _results;
+    }
+}
