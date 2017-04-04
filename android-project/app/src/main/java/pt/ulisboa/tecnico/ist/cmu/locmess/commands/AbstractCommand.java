39076ee638bd099abe6dc6a40ea061c4b15f60b7
Jorge Heleno <jorge.heleno@rnl.tecnico.ulisboa.pt>
2017-04-04 15:10:41 +0100

    Adds RegisterUserCommand and AbstractCommand
    Closes #22, Refers to #24

---
+++ android-project/app/src/main/java/pt/ulisboa/tecnico/ist/cmu/locmess/commands/AbstractCommand.java
@@ -0,0 +1,69 @@
+package pt.ulisboa.tecnico.ist.cmu.locmess.commands;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import pt.ulisboa.tecnico.ist.cmu.locmess.exception.AlreadyRequestedException;
+import pt.ulisboa.tecnico.ist.cmu.locmess.exception.NotYetRequestedException;
+
+/**
+ * Created by jorge on 03/04/17.
+ */
+
+public abstract class AbstractCommand {
+
+    private final String SERVERADDR="pikachu.rnl.tecnico.ulisboa.pt";
+    private final Integer SERVERPORT=31000;
+    private String _endpoint,_args,_response;
+    protected boolean _requestState=false;
+
+    protected AbstractCommand(String endpoint, String args){
+        _endpoint=endpoint;
+        _args=args;
+    }
+
+    public AbstractCommand(String endpoint){
+        _endpoint=endpoint;
+        _args="";
+    }
+
+    public void execute() throws IOException, AlreadyRequestedException {
+        if(_requestState){
+            throw new AlreadyRequestedException();
+        }
+        URL url=new URL(SERVERADDR+":"+SERVERPORT.toString()+"/"+_endpoint+"?"+_args);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        String response ="";
+
+        while ((inputLine = in.readLine()) != null) {
+            response+=inputLine;
+        }
+        in.close();
+        _response=response;
+        _requestState=true;
+    }
+
+    public String getResponse() throws IOException, AlreadyRequestedException {
+        if(!_requestState){
+            execute();
+        }
+        return _response;
+
+    }
+
+    public String getAddr(){
+        return SERVERADDR;
+    }
+
+    public Integer getPort(){
+        return SERVERPORT;
+    }
+
+}
---
+++ android-project/app/src/main/java/pt/ulisboa/tecnico/ist/cmu/locmess/commands/RegisterUserCommand.java
@@ -0,0 +1,32 @@
+package pt.ulisboa.tecnico.ist.cmu.locmess.commands;
+
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import pt.ulisboa.tecnico.ist.cmu.locmess.JsonParser;
+import pt.ulisboa.tecnico.ist.cmu.locmess.exception.AlreadyRequestedException;
+
+/**
+ * Created by jorge on 03/04/17.
+ */
+
+public class RegisterUserCommand extends AbstractCommand {
+
+    private static final String _endpoint="registerUser";
+
+
+    public RegisterUserCommand(String username, String password){
+        super(_endpoint,"username="+username+"&password="+password);
+    }
+
+    @Override
+    public void execute() throws IOException, AlreadyRequestedException {
+        super.execute();
+    }
+
+    public boolean successfulRequest() throws IOException, AlreadyRequestedException, JSONException {
+        return JsonParser.getValue(super.getResponse(),"response").equals("success");
+    }
+}
