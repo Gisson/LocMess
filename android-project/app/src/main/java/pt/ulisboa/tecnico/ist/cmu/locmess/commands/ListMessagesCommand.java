fdad5f501484d732dde4bd58a637c4829e304b8e
Jorge Heleno <jorge.heleno@rnl.tecnico.ulisboa.pt>
2017-04-04 15:14:46 +0100

    Adds ListMessagesCommand
    Refers to #32

---
+++ android-project/app/src/main/java/pt/ulisboa/tecnico/ist/cmu/locmess/commands/ListMessagesCommand.java
@@ -0,0 +1,48 @@
+package pt.ulisboa.tecnico.ist.cmu.locmess.commands;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import pt.ulisboa.tecnico.ist.cmu.locmess.exception.AlreadyRequestedException;
+
+/**
+ * Created by jorge on 03/04/17.
+ */
+
+public class ListMessagesCommand extends AbstractCommand {
+
+    //TODO
+
+    private static final String _endpoint="listMessages";
+    private HashMap<String,HashMap<String,String>> _results=null;
+
+    public ListMessagesCommand(String token) {
+        super(_endpoint,"token="+token);
+    }
+
+    @Override
+    public void execute() throws IOException, AlreadyRequestedException {
+        super.execute();
+    }
+
+    public Map<String,HashMap<String,String>> getResults() throws IOException, AlreadyRequestedException, JSONException {
+        if(_results==null){
+            JSONObject obj=new JSONObject(getResponse());
+            JSONArray arr=obj.getJSONArray("messages");
+            _results=new HashMap<String,HashMap<String,String>>();
+            HashMap<String,String> references;
+            for(int i=0; i<arr.length(); i++){
+                references=new HashMap<String,String>();
+                references.put("Author",arr.getJSONObject(i).getJSONObject("references").getString("Author"));
+                references.put("Content",arr.getJSONObject(i).getJSONObject("references").getString("Content"));
+                _results.put(references.put("Id",arr.getJSONObject(i).getJSONObject("messages").getString("Id")),references);
+            }
+        }
+        return _results;
+    }
+}
