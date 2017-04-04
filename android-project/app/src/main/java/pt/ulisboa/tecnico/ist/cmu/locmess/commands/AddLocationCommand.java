8fcbad67c7c8162f6e12777360b4a214c1b89c37
Jorge Heleno <jorge.heleno@rnl.tecnico.ulisboa.pt>
2017-04-04 15:13:52 +0100

    Adds AddLocationCommand
    Refers to #27

---
+++ android-project/app/src/main/java/pt/ulisboa/tecnico/ist/cmu/locmess/commands/AddLocationCommand.java
@@ -0,0 +1,46 @@
+package pt.ulisboa.tecnico.ist.cmu.locmess.commands;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import pt.ulisboa.tecnico.ist.cmu.locmess.JsonParser;
+import pt.ulisboa.tecnico.ist.cmu.locmess.exception.AlreadyRequestedException;
+
+/**
+ * Created by jorge on 03/04/17.
+ */
+
+public class AddLocationCommand extends AbstractCommand {
+
+    private static final String _endpoint="addLocation";
+    private String _response=null;
+
+    //This big line of code is just due to java being obligated to use super as first line of code of a constructor.
+    public AddLocationCommand(String token, String name,String latitude,String longitude,String radius,String ssid,String bssid) {
+        super(_endpoint,"token="+token+"name="+name+((hasGpsCoordinates(latitude,longitude,radius)) ?  //Everything starts with name, if latitude, longitude and radius are defined
+                "&latitude="+latitude+"&longitude="+longitude+"&radius="+radius:        // Use longitude, latitude and radius
+                (hasBssid(bssid))?"" +                                                  // else check if bssid is defined
+                        "&bssid="+bssid:                                                // if it is then use it
+                        "&ssid="+ssid));                                                // last case scenario, the ssid MUST be defined
+    }
+
+    @Override
+    public void execute() throws IOException, AlreadyRequestedException {
+        super.execute();
+    }
+
+    private static boolean hasGpsCoordinates(String latitude,String longitude,String radius){
+        return !(latitude.equals("") && longitude.equals("") && radius.equals(""));
+    }
+    private static boolean hasBssid(String bssid){
+        return !bssid.equals(bssid);
+    }
+    private static boolean hasSsid(String ssid){
+        return !ssid.equals(ssid);
+    }
+
+    public boolean successfulRequest() throws IOException, AlreadyRequestedException, JSONException {
+            return JsonParser.getValue(super.getResponse(),"response").equals("success");
+    }
+}
