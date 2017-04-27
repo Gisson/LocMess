fae6fd274ea8e238548a79258d66b036d4457a27
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2017-04-27 11:56:31 +0100

    feat: add topic to user profile
    closes #30

--- android-project/app/src/main/java/pt/ulisboa/tecnico/ist/cmu/locmess/MyProfile.java
+++ android-project/app/src/main/java/pt/ulisboa/tecnico/ist/cmu/locmess/MyProfile.java
@@ -7,11 +7,14 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import pt.ulisboa.tecnico.ist.cmu.locmess.adapters.TopicsListAdapter;
+import pt.ulisboa.tecnico.ist.cmu.locmess.commands.AddKeyCommand;
+import pt.ulisboa.tecnico.ist.cmu.locmess.dto.TopicDto;
 
 public class MyProfile extends AppCompatActivity {
     private static final String TAG = "MyProfileActivity";
@@ -19,6 +22,7 @@ public class MyProfile extends AppCompatActivity {
     private static final String[] mockTopics = {"os=linux", "university=ist", "club=none"};
     private ArrayList<String> topics = new ArrayList<String>(Arrays.asList(mockTopics));
     private TopicsListAdapter topicsAdapter;
+    private LocMessManager manager = LocMessManager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +41,29 @@ public class MyProfile extends AppCompatActivity {
             }
         });
 
-        topicsAdapter = new TopicsListAdapter(topics, this);
+        TopicsListAdapter.NewItemCallback newItemCallback = new TopicsListAdapter.NewItemCallback() {
+            @Override
+            public void onNewItem(final int index) {
+                String token = manager.getToken();
+                String text = topics.get(index);
+                AddKeyCommand request = new AddKeyCommand(token, new TopicDto(text));
+                manager.executeAsync(request, new LocMessManager.CompleteCallback() {
+                    @Override
+                    public void OnComplete(final boolean result, String message) {
+                        int duration = Toast.LENGTH_SHORT;
+                        if(result){
+                            message = "topic added";
+                        } else {
+                            topics.remove(index); // FIXME the index may have changed!
+                            topicsAdapter.notifyDataSetChanged();
+                            duration = Toast.LENGTH_LONG;
+                        }
+                        Toast.makeText(MyProfile.this, message, duration).show();
+                    }
+                });
+            }
+        };
+        topicsAdapter = new TopicsListAdapter(topics, this, newItemCallback);
 
         ListView lvTopics = (ListView)findViewById(R.id.lvTopics);
         lvTopics.setAdapter(topicsAdapter);
